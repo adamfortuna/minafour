4c10087c1db07905f6ff65774e27ca1f8647a426
Adam Fortuna <adam@fortuna.name>
2018-09-24 18:45:10 -0600

    Added front page and functions

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Front page template
+ *
+ * @package      Minafi
+ * @author       Adam Fortuna
+ * @link         https://minafi.com
+ * @copyright    Copyright (c) 2018, Adam Fortuna
+ * @license      GPL-2.0+
+ */
+
+genesis();
---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Theme customizations
+ *
+ * @package      Minafour
+ * @author       Adam Fortuna
+ * @link         https://minafi.com
+ * @copyright    Copyright (c) 2018, Adam Fortuna
+ * @license      GPL-2.0+
+ */
+
+// Load child theme textdomain.
+load_child_theme_textdomain( 'minafour' );
+
+add_action( 'genesis_setup', 'minafour_setup' );
+/**
+ * Theme setup.
+ *
+ * Attach all of the site-wide functions to the correct hooks and filters. All
+ * the functions themselves are defined below this setup function.
+ *
+ * @since 1.0.0
+ */
+function minafour_setup() {
+
+}
